4e502133fead921797834a727b42b63f0bdf085d
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-10-16 23:40:56 +0800

    Update start-server.js

--- app/setup/start-server.js
+++ app/setup/start-server.js
@@ -2,7 +2,7 @@ import { PORT } from "../config/env"
 
 export default function (app) {
 
-    var server = app.listen(PORT, function () {
+    var server = app.listen(PORT, "0.0.0.0", function () {
         var host = server.address().address
         var port = server.address().port
         console.log("App listening at http://%s:%s", host, port)
