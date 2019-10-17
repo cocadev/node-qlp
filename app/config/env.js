93544f7278fc3b4e7458ad294fbad1f104659794
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-10-17 11:25:34 +0800

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const NODE_ENV = 'development'
-export const PORT = '3400'
+export const PORT = '3306'
 export const DATABASE = 'qx_qlp'
 export const USERNAME = 'qlp_users'
 export const PASSWORD = 'KuttA_2021_DhakA'
-export const HOST = 'localhost'
+export const HOST = '0.0.0.0'
 export const DIALECT = 'mysql'
 export const SECRET = 'grokonez-super-secret-key'
 export const ROLES = ['INVESTOR', 'ADMIN']
